2328b6238bb35e349e0c18799b005fecb2b8436f
Esther241 <esther.dinh@gmail.com>
2021-06-02 11:18:30 +0200

    add popup liste pointage

--- mon-app/src/User/Components/Dashboard.jsx
+++ mon-app/src/User/Components/Dashboard.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
 import Kpi from "./Kpi";
-import Footer from "../../Main/Footer/Footer";
 
 import KmUsers from "./KmUsers";
+import PopupList from "./PopupList";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,10 +20,7 @@ function Dashboard() {
     <div>
       <KmUsers />
       <Kpi />
-      <Button variant="contained" color="primary">
-        Liste pointage
-      </Button>
-      <Footer />
+      <PopupList />
     </div>
   );
 }
