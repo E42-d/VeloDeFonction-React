97a7f8add7567dd0b1ad5f106f3569d516eb8f33
E42-d <marc.desideri@hotmail.fr>
2021-06-02 14:58:14 +0200

    Scan component

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import "./App.css";
+import Background from "./Background";
+import Scanne from "./Scanne";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <div style={{width:"100%",}} className = "some-class" >
+<Background/>
+      <img src={"/LogoVF1.png"} style={{width:"100%",margin:"0",height:"90%",zIndex:"-1",backgroundColor:"rgba(255, 0, 0, 0.0"}}/>
+<Scanne/>
+
+   </div>
+  
   );
 }
 
